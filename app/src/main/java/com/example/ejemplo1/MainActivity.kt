ea64bdaacd9837814d0286b00a8c122a7a7c13e5
alohonda <aloondravic1@outlook.com>
2025-02-17 13:47:32 -0600

    Practica de imagenes 17 Febrero 2025

--- app/src/main/java/com/example/ejemplo1/MainActivity.kt
+++ app/src/main/java/com/example/ejemplo1/MainActivity.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo1.ui.theme.Ejemplo1Theme
+import androidx.compose.ui.layout.ContentScale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,30 +36,90 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Ejemplo1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Feliz Cumpleaños Jose",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                GreetingPreview()
             }
         }
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = name,
-        fontSize = 100.sp,
-        lineHeight = 116.sp
+fun GreetingPreview(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        CircleImageView()
+    }
+    //SimpleImage()
+}
+
+@Composable
+fun SimpleImage() {
+    Image(
+        painter = painterResource(id = R.drawable.andy_rubin),
+        contentDescription = "Andy Rubin",
+        modifier = Modifier.fillMaxWidth()
     )
 }
 
-@Preview(showBackground = true)
+@Composable fun CircleImageView() {
+    Image( painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Circle Image",
+        contentScale = ContentScale.Crop,
+            alignment = Alignment.Center,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(CircleShape)
+            .border(5.dp, Color.Gray, CircleShape)
+    )
+}
+
 @Composable
-fun GreetingPreview() {
-    Ejemplo1Theme {
-        Greeting("TEC Laguna")
-    }
+fun RoundCornerImageView() {
+    Image(
+        painter = painterResource(R.drawable.andy_rubin),
+        contentDescription = "Round corner image",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(128.dp)
+            .clip(RoundedCornerShape(10))
+            .border(5.dp, Color.Gray, RoundedCornerShape(10))
+    )
+}
+
+@Composable
+fun ImageWithBackgroundColor() {
+    Image(
+        painter = painterResource(id = R.drawable.cart),
+        contentDescription = "",
+        modifier = Modifier
+            .size(200.dp)
+            .background(Color.DarkGray)
+            .padding(20.dp)
+    )
+}
+
+@Composable
+fun ImageWithTint() {
+    Image(
+        painter = painterResource(id = R.drawable.cart),
+        contentDescription = "",
+        colorFilter = ColorFilter.tint(Color.Red),
+        modifier = Modifier
+            .size( 200.dp)
+    )
+}
+
+@Composable
+fun InsideFit() {
+    Image(
+        painter = painterResource(id = R.drawable.andy_rubin),
+        contentDescription = "",
+        modifier = Modifier
+            .size(150.dp)
+            .background(Color.LightGray),
+        contentScale = ContentScale.Inside
+    )
 }
